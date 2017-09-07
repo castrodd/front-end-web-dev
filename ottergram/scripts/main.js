37132ebeca722a201c61432feb5c2b77ccea0a45
Daniel Castro <castrodd@gmail.com>
2017-09-06 20:17:34 -0400

    CSS transitions

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,6 +1,7 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]'
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]'
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]'
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
 
 function setDetails(imageURL, titleText) {
   'use strict';
@@ -26,9 +27,27 @@ function setDetailsFromThumb(thumbnail) {
   setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
 
+function hideDetails() {
+  'use strict';
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+  'use strict';
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+}
+
 document.addEventListener('click', function(evt) {
   evt.preventDefault();
   if (evt.target.className === 'thumbnail-image') {
     setDetailsFromThumb(evt.target.parentElement);
+  }
+  showDetails();
+})
+
+document.addEventListener('keyup', function(evt) {
+  evt.preventDefault();
+  if (evt.keyCode === 27) {
+    hideDetails();
   }
 })
